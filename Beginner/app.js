3dc9805e6924c5e211fa584c4800a6eb416b8181
zeenayouhan <zinayouhan33@gmail.com>
2020-03-24 11:46:14 +0530

    NodeSayHello

--- Beginner/app.js
+++ Beginner/app.js
@@ -1,14 +1,5 @@
-const http = require('http');
+function SayHello(name){
+    console.log('Hello '+name)
+}
 
-const hostname="127.0.0.1";
-const port=3000;
-
-const server=http.createServer((req,res)=>{
-    res.statusCode=200;
-    res.setHeader('Content-Type','text/plain');
-    res.end('Hello World');
-});
-
-server.listen(port,hostname,()=>{
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+SayHello('Zeena');
