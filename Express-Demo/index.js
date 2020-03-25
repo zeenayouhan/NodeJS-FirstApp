a158a2d01249c62ea1558797cd580aa8957cd478
zeenayouhan <zinayouhan33@gmail.com>
2020-03-25 20:38:28 +0530

    Express-Demo-Delelte,Put

--- Express-Demo/index.js
+++ Express-Demo/index.js
@@ -24,13 +24,16 @@ app.post('/api/course',(req,res)=>{
 
         name:Joi.string().min(3).required()
     }
-    const result=Joi.validate(req.body,schema);
-    console.log(result);
-    //input validating
-    if(!req.body.name || req.body.name.length<3){
-        res.status(400).send("Invalid name");
+    const {error}=validateCourse(req.body);  //result.error
+      if(error){
+        res.status(400).send(error.details[0].message);
         return ;
     }
+    //input validating
+   // if(!req.body.name || req.body.name.length<3){
+     //   res.status(400).send("Invalid name");
+       // return ;
+    //}
     const course={
         id: courses.length+1,
         name: req.body.name
@@ -39,6 +42,31 @@ app.post('/api/course',(req,res)=>{
     res.send(courses);
 })
 
+app.put('/api/course/:id',(req,res)=>{
+    var course = courses.find(c => c.id===parseInt(req.params.id))
+    if(!course) res.status(404).send("The course with given id was not found");
+
+    const result=validateCourse(req.body);
+    const {error}=validateCourse(req.body);  //result.error
+      if(error){
+        res.status(400).send(error.details[0].message);
+        return ;
+    }
+    
+    course.name=req.body.name;
+    res.send(courses);
+
+    
+
+})
+function validateCourse(course){
+    const schema={
+
+        name:Joi.string().min(3).required()
+    }
+    return Joi.validate(course,schema);
+}
+  
 app.get('/api/course/:id',(req,res)=>{
     var course = courses.find(c => c.id===parseInt(req.params.id))
     if(!course) res.status(404).send("The course with given id was not found");
@@ -50,4 +78,15 @@ const port = process.env.PORT || 3000;
 
 app.listen(port,()=>{
     console.log(`Listening to the ${port}`);
+})
+
+app.delete('/api/course/:id',(req,res)=>{
+    var course = courses.find(c => c.id===parseInt(req.params.id))
+    if(!course) res.status(404).send("The course with given id was not found");
+
+    const index=courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
+
 })
