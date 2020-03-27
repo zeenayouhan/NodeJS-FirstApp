aab994697dc02c0c6022797c253fdda5b8ff1fcf
zeenayouhan <zinayouhan33@gmail.com>
2020-03-27 13:19:23 +0530

    async-promises

---
+++ asychn-demo/promises.js
@@ -0,0 +1,13 @@
+const p = new Promise((resolve,reject)=>{
+    //kick off some async work
+    setTimeout(()=>{
+        //resolve(2);
+        reject(new Error('message'));
+
+    },2000);
+    
+   
+
+});
+p.then(result =>console.log('result: ',result))
+.catch(err=>console.log('Error',err.message));
