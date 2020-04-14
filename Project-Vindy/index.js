4b4dd5c51b8a3c61b25c5d449a6068130651e2c2
zeenayouhan <zinayouhan33@gmail.com>
2020-04-14 10:45:13 +0530

    Project-vindy-post

--- Project-Vindy/index.js
+++ Project-Vindy/index.js
@@ -11,10 +11,27 @@ const genres = [
   ];
 
 app.get('/api/genres',(req,res)=>{
-    res.send(JSON.stringify(genres))
+    res.send(genres);
 
 
 })
+app.post('/api/generes',(req,res)=>{
+    const schema={
+        name:Joi.string().min(4).required()
+    }
+    const {error}= validateCourse(req.body);
+    if(error){
+        return res.status(404).send(error.details[0].message);
+    }
+
+    const genre={
+        id: genres.length+1,
+        name:req.body.name
+    }
+    genres.push(genre);
+    res.send(genres);
+})
+
 app.listen(3000,()=>{
     console.log("Connected");
 
